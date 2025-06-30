4cfc7ccfba2004ed14891234e455c359d4835d3f
parmeetkaur0 <157615690+parmeetkaur0@users.noreply.github.com>
2025-07-01 00:21:30 +0530

    Update firebase.config.js

--- client/src/config/firebase.config.js
+++ client/src/config/firebase.config.js
@@ -3,9 +3,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeK3ZMCOIkn1KySv9pJ3JQONCWqP7JaYI",
-  authDomain: "ad-detection-a8205.firebaseapp.com",
-  projectId: "ad-detection-a8205",
+ apiKey: "AIzaSyCeBbcEoshdgwmhS7BNpftzhpFzTYIsXR0",
+  authDomain: "shopbyte-ecommerce.firebaseapp.com",
+  projectId: "shopbyte-ecommerce",
   storageBucket: "ad-detection-a8205.firebasestorage.app",
   messagingSenderId: "638053861414",
   appId: "1:638053861414:web:3b0d4bb666c6479926bee7",
