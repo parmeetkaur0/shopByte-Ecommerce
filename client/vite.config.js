6b920a9a6d59b59d5a0cadb9350485ade9c478cd
Parmeet Kaur <kparmeet300@gmail.com>
2025-07-01 20:16:31 +0530

    add vercel.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,4 +10,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist"
+  },
+  server: {
+    port: 5173
+  },
+  base: "/",
 });
